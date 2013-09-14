aaea25c8adf2f649e7f944e8a59c769ec3934df0
Kirill Sibirev <l0kix2@gmail.com>
2013-09-14 17:01:33 +0400

    Execute functions in bashrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,15 @@
+# packages
 apt-get update
 apt-get install -y build-essential debhelper dh-make mc
+
+# some debian defaults
+cat >>/home/vagrant/.bashrc <<EOF
+DEBEMAIL="sibirev@yandex-team.ru"
+DEBFULLNAME="Kirill Sibirev"
+export DEBEMAIL DEBFULLNAME
+EOF
+
+# useful functions
+cat >>/home/vagrant/.bashrc <<EOF
+source /vagrant/bash_functions/diff_fs.sh
+EOF
